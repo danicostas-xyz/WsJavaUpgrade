ef11bd4c3122e8d59c751759638714479207295f
fdepablo <f.depablo.lobo@gmail.com>
2024-04-25 12:52:34 +0200

    modificacion de clase jefe

--- ProgActividad22/src/entidad/JefeDeProyecto.java
+++ ProgActividad22/src/entidad/JefeDeProyecto.java
@@ -2,39 +2,28 @@ package entidad;
 
 public class JefeDeProyecto extends Empleado {
 	
-	private double incentivo;
-	
-	
+	private double incentivo;	
 
 	@Override
 	public String toString() {
 		return "JefeDeProyecto [incentivo=" + incentivo + ", " + super.toString() + "]";
 	}
 
-
-
 	public double getIncentivo() {
 		return incentivo;
 	}
-
-
 
 	public void setIncentivo(double incentivo) {
 		this.incentivo = incentivo;
 	}
 
-
-
 	public JefeDeProyecto() {
 		
 	}
-
 
 	@Override
 	public double salarioTotal() {
 		return this.getSueldoBase() + this.incentivo;
 	}
-	
-	
 
 }
